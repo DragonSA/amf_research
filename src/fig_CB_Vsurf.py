597d59f72428aa64e67c1de9c890f50abb157c94
David Naylor <naylor.b.david@gmail.com>
2012-09-16 10:03:01 +0200

    Make figure producing code slightly neater.
    Refactor code into functions, and call main() if the script is run directly.
    This will prevent code, and work, from leaking if the fig_ modules are imported
    by accident.

--- src/fig_CB_Vsurf.py
+++ src/fig_CB_Vsurf.py
@@ -1,23 +1,21 @@
-
+"""
+Graph showing the pricing surface of the example convertible bond.
+"""
 import matplotlib
 #matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
-from model import FDEModel, WienerJumpProcess
-from payoff import CallE
+from convertible_bond import dS_total as dS, payoff
+from model import FDEModel
 
-dS = WienerJumpProcess(0.1, 0.1)
-# Semi-annual payments of 1
-T = 3
-V = CallE(T, 100)
-model = FDEModel(T * 200, dS, V)
-P = model.price(0, 200, 200)
-X, Y = np.meshgrid(P.S, P.t)
-Z = np.array(P.V)
-
-if __name__ == "__main__":
+def main():
+    N = 800
+    model = FDEModel(N, dS, payoff)
+    P = model.price(0, 200, 200)
+    X, Y = np.meshgrid(P.S, P.t)
+    Z = np.array(P.V)
     fig = plt.figure()
     ax = fig.add_subplot(111, projection="3d")
     ax.plot_surface(X, Y, Z, linewidth=0)
@@ -25,3 +23,6 @@ if __name__ == "__main__":
     ax.set_ylabel("Time")
     ax.set_zlabel("Portfolio Value")
     plt.show()
+
+if __name__ == "__main__":
+    main()
--- src/fig_mk12.py
+++ src/fig_mk12.py
@@ -9,29 +9,34 @@ import numpy
 numpy.seterr(divide="ignore")
 
 from convertible_bond import dS_total as dS, payoff, B
-from model import BinomialModel
+from model import BinomialModel, FDEModel
 
-dS.sigma = 0.25
-dS.lambd_ = 0.062
-dSv = copy.copy(dS)
-dSv.lambd_ = lambda S: 0.062 * (S / 50)**-0.5
-dSv.cap_lambda = True
-B.R = 0.4
-
-if __name__ == "__main__":
+def main():
     N = 250
-    S = range(120)
+    S = range(121)
     P = [[], []]
+    dS.sigma = 0.25
+    dS.lambd_ = 0.062
+    dSv = copy.copy(dS)
+    dSv.lambd_ = lambda S: 0.062 * (S / 50)**-0.5
+    dSv.cap_lambda = True
+    B.R = 0.4
     model1 = BinomialModel(N, dS, payoff)
     model2 = BinomialModel(N, dSv, payoff)
+    model3 = FDEModel(N, dS, payoff)
     for s in S:
         P[0].append(float(model1.price(s)))
         P[1].append(float(model2.price(s)))
     plt.plot(S, P[0])
     plt.plot(S, P[1])
+    plt.plot(S, model3.price(0, 120, 120).V[0])
     plt.ylim([40, 160])
     plt.xlabel("Share price")
     plt.ylabel("Convertible Bond Price")
     plt.title("Convertible Bond Price Profile, N = %i, R = %i%%" % (N, B.R * 100))
-    plt.legend(["Constant Hazard Rate", "Synthesis Hazard Rate"])
+    plt.legend(["Constant Hazard Rate", "Synthesis Hazard Rate", "FDE"])
     plt.savefig("../common/fig_mk12.svg")
+    #plt.show()
+
+if __name__ == "__main__":
+    main()
