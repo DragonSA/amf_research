8ccc48c7c6a474ca3f3f87297ab473af3fe5bb4c
David Naylor <naylor.b.david@gmail.com>
2012-09-18 08:57:42 +0200

    Refine convertible bond implementation to behave like AFV03.
    The put (P) and call (C) provisions use "dirty" strike prices.  The outstanding
    coupon payment, on a simple linear basis, is added to the "clean" strike price.
    On coupon payment times it is assumed the coupon is payed before the options
    are evaluated, thus no accrued coupons are needed.
    
    The Annuity's (B) final coupon is payed as part of the nominal amount, not as
    an explicit coupon.  This reduced the final payoff when the conversion option
    takes affect.

--- src/convertible_bond.py
+++ src/convertible_bond.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from model import WienerJumpProcess, BinomialModel, FDEModel, Payoff
-from payoff import Annuity, CallA, CallVR, PutV, Stack, Time
+from payoff import Annuity, CallA, CallVR, PutV, Stack, Time, VariableStrike
 
 __all__ = ["dS_total", "dS_partial", "B", "P", "C", "S", "payoff", "T"]
 
@@ -23,17 +23,59 @@ dS = WienerJumpProcess(r=0.05, sigma=0.2)
 #       Nominal value = 100
 #       Semi-annual coupon = 4
 #       Recovery factor = 0
+class Annuity(Annuity):
+    """Customised Annuity where coupon is payed as part of nominal."""
+    def coupon(self, t):
+        if t != self.T:
+            return super(Annuity, self).coupon(t)
+        return 0.0
+
+    def terminal(self, S):
+        V = super(Annuity, self).terminal(S)
+        if self.T in self.times:
+            V += self.C
+        return V
 B = Annuity(T, np.arange(0.5, T + 0.5, 0.5), 4, 100, 0)
 
 # American put option on portfolio
 #       Strike = 105
 #       Time = 3
-P = Time(PutV(T, 105), [3])
+class Put(PutV, VariableStrike):
+    def __init__(self, T, K, B):
+        super(Put, self).__init__(T, K)
+        self._B = B
+
+    def transient(self, t, V, S):
+        ti = 0
+        accC = 0
+        for i in self._B.times:
+            if i > t:
+                accC = self._B.C * (t - ti) / (i - ti)
+                break
+            ti = i
+        with self._strike(self.K + accC):
+            return super(Put, self).transient(t, V, S)
+P = Time(Put(T, 105, B), [3])
 
 # Reversed American call option on portfolio
 #       Strike = 110
 #       Time = [2, 5]
-C = Time(CallVR(T, 110), [(2, 5)])
+class Call(CallVR, VariableStrike):
+    def __init__(self, T, K, B):
+        super(Call, self).__init__(T, K)
+        self._B = B
+
+    def transient(self, t, V, S):
+        ti = 0
+        accC = 0
+        for i in self._B.times:
+            if i > t:
+                accC = self._B.C * (t - ti) / (i - ti)
+                break
+            ti = i
+        with self._strike(self.K + accC):
+            return super(Call, self).transient(t, V, S)
+C = Time(Call(T, 110, B), [(2, 5)])
 
 # Stock option (conversion option into stock for portfolio)
 S = CallA(T, 0)
@@ -47,8 +89,10 @@ payoff = Stack([B, P, C, S])
 
 if __name__ == "__main__":
     N = 3200
-    dS = dS_total
+    dS = dS
     S0 = 100
+    Sl = 0
+    Su = 200
     print "BINOMIAL"
     print float(BinomialModel(N, dS, Stack([B])).price(S0)), "(B)"
     print float(BinomialModel(N, dS, Stack([B, S])).price(S0)), "(B+S)"
@@ -56,8 +100,8 @@ if __name__ == "__main__":
     print float(BinomialModel(N, dS, Stack([B, P, S])).price(S0)), "(B+P+S)"
     print float(BinomialModel(N, dS, Stack([B, P, C, S])).price(S0)), "(B+P+C+S)"
     print "\nFDE"
-    print FDEModel(N, dS, Stack([B])).price(0, 200, 200).V[0][100], "(B)"
-    print FDEModel(N, dS, Stack([B, S])).price(0, 200, 200).V[0][100], "(B+S)"
-    print FDEModel(N, dS, Stack([B, C, S])).price(0, 200, 200).V[0][100], "(B+C+S)"
-    print FDEModel(N, dS, Stack([B, P, S])).price(0, 200, 200).V[0][100], "(B+P+S)"
-    print FDEModel(N, dS, Stack([B, P, C, S])).price(0, 200, 200).V[0][100], "(B+P+C+S)"
+    print FDEModel(N, dS, Stack([B])).price(Sl, Su, N).V[0][N // 2], "(B)"
+    print FDEModel(N, dS, Stack([B, S])).price(Sl, Su, N).V[0][N // 2], "(B+S)"
+    print FDEModel(N, dS, Stack([B, C, S])).price(Sl, Su, N).V[0][N // 2], "(B+C+S)"
+    print FDEModel(N, dS, Stack([B, P, S])).price(Sl, Su, N).V[0][N // 2], "(B+P+S)"
+    print FDEModel(N, dS, Stack([B, P, C, S])).price(Sl, Su, N).V[0][N // 2], "(B+P+C+S)"
