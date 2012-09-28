52583f2e4f13c31a47ecefc56fe87bd18483ef3b
David Naylor <naylor.b.david@gmail.com>
2012-09-28 15:10:56 +0200

    Remove the binomial model for the lambda_eta graph.
    The kink was solved by increasing the upper stock bound.  With this there is no
    need for a comparative figure.

--- src/fig_lambda_eta.py
+++ src/fig_lambda_eta.py
@@ -7,14 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from convertible_bond import dS, payoff, T
-from model import BinomialModel, FDEModel, ImplicitScheme
-from payoff import CallA
-
-def label(plt, model, name):
-    plt.set_title("%s pricing model" % model)
-    plt.set_xlabel("$\\eta$")
-    plt.set_ylabel("Price at $S=100$")
-    plt.legend(name, loc=2)
+from model import FDEModel
 
 def main():
     Sl = 0
@@ -25,26 +18,19 @@ def main():
     Sk = K * (S - Sl) / (Su - Sl)
     ETA = np.linspace(0, 1, 65)
     name = ["No default"]
-    payoff = CallA(T, 100)
-    binom = BinomialModel(N, dS, payoff)
     fde = FDEModel(N, dS, payoff)
-    plt_binom = plt.subplot(1, 2, 1)
-    plt_fde = plt.subplot(1, 2, 2, sharex=plt_binom, sharey=plt_binom)
-    plt_binom.plot(ETA, [float(binom.price(S))] * len(ETA))
-    plt_fde.plot(ETA, [fde.price(Sl, Su, K).V[0][Sk]] * len(ETA))
-    for lambd_ in (0.1, 0.2, 0.3):
+    plt.plot(ETA, [fde.price(Sl, Su, K).V[0][Sk]] * len(ETA))
+    for lambd_ in (0.01, 0.02, 0.03):
         dS.lambd_ = lambd_
-        binomV = []
-        fdeV = []
+        V = []
         for eta in ETA:
             dS.eta = eta
-            binomV.append(float(binom.price(S)))
-            fdeV.append(fde.price(Sl, Su, K).V[0][Sk])
-        plt_binom.plot(ETA, binomV)
-        plt_fde.plot(ETA, fdeV)
-        name.append("$\\lambda = %.1f$" % (lambd_))
-    label(plt_binom, "Binomial", name)
-    label(plt_fde, "FDE", name)
+            V.append(fde.price(Sl, Su, K).V[0][Sk])
+        plt.plot(ETA, V)
+        name.append("$\\lambda = %i\\%%$" % (lambd_ * 100))
+    plt.xlabel("$\\eta$")
+    plt.ylabel("Price at $S=100$")
+    plt.legend(name, loc=3)
     plt.savefig("../common/fig_lambda_eta.png")
     plt.savefig("../common/fig_lambda_eta.svg")
     #plt.show()
