1588f8e02e24e04c155be071d7b7a55c6789c3ac
David Naylor <naylor.b.david@gmail.com>
2012-09-28 15:08:48 +0200

    Refine the MK12 graph.
    Use the full stock scale provided by the FDEModel and include the value for
    S=0, as this is analytically solvable.

--- src/fig_mk12.py
+++ src/fig_mk12.py
@@ -12,12 +12,12 @@ from convertible_bond import dS_total as dS, payoff, B, T
 from model import FDEModel
 
 def main():
-    S = np.arange(121)
+    S = np.linspace(0, 120, 120 * 8 + 1)
     Sl = 0
     Su = 200
     N = 128 * T
     K = 8 * (Su - Sl)
-    Sk = K * (S - Sl) / (Su - Sl)
+    Sk = (K * (S - Sl) / (Su - Sl)).astype(int)
     dS.sigma = 0.25
     dS.lambd_ = 0.062
     dSv = copy.copy(dS)
@@ -27,11 +27,10 @@ def main():
     model1 = FDEModel(N, dS, payoff)
     model2 = FDEModel(N, dSv, payoff)
     plt.plot(S, model1.price(Sl, Su, K).V[0][Sk])
-    plt.plot(S[1:], model2.price(Sl + 1, Su, K - 8).V[0][Sk[:-1]])
+    plt.plot(S, np.append(B.R * B.N, model2.price(Sl + 1, Su, K - 8).V[0][Sk[:-1]]))
     plt.ylim([40, 160])
     plt.xlabel("Stock Price")
     plt.ylabel("Convertible Bond Price")
-    plt.title("Convertible Bond Price Profile, N = %i, R = %i%%" % (N, B.R * 100))
     plt.legend(["Constant $\\lambda$", "Synthesis $\\lambda$"], loc=2)
     plt.savefig("../common/fig_mk12.png")
     plt.savefig("../common/fig_mk12.svg")
