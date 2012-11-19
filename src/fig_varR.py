a99bc8877ef7e0b76c7d269c6026de6074ba4ce1
David Naylor <naylor.b.david@gmail.com>
2012-11-19 13:49:53 +0200

    Add figure "varR" comparing R=0 and R=104 and price surface of R=104.

---
+++ src/fig_varR.py
@@ -0,0 +1,42 @@
+"""
+Graph comparing different R and price surface for zero-redemption.
+"""
+import sys
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+
+from convertible_bond import dS_total as dS, payoff, A, T
+from model import FDEModel
+from plot import plot_model
+
+def main():
+    S = np.linspace(0, 200, 200 * 8 + 1)
+    Sl = 0
+    Su = 250
+    N = 128 * T
+    K = 8 * (Su - Sl)
+    Sk = (K * (S - Sl) / (Su - Sl)).astype(int)
+
+    model = FDEModel(N, dS, payoff)
+    fig = plt.figure()
+    fig.set_figwidth(1.8 * fig.get_figwidth())
+
+    ax = fig.add_subplot(1, 2, 1)
+    ax.plot(S, model.price(Sl, Su, K).V[0][Sk])
+    A.N = -A.C
+    ax.plot(S, model.price(Sl, Su, K).V[0][Sk])
+    plt.xlabel("Stock Price")
+    plt.ylabel("Convertible Bond Price")
+    plt.legend(["$R = 104$", "$R = 0$"])
+
+    ax = fig.add_subplot(1, 2, 2, projection="3d")
+    plot_model(ax, dS, payoff)
+    plt.savefig("../common/fig_varR.pdf")
+    #plt.show()
+
+
+if __name__ == "__main__":
+    main()
